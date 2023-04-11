5e3e1b1224d12ab0fb848be43411b3e520ecfaa6
gymnopediese <kality.of.life@gmail.com>
2023-04-11 12:55:39 +0200

    m

--- template/srcs/header.h
+++ template/srcs/header.h
@@ -6,11 +6,13 @@
 /*   By: albaud <albaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 12:49:37 by albaud            #+#    #+#             */
-/*   Updated: 2023/04/11 12:49:58 by albaud           ###   ########.fr       */
+/*   Updated: 2023/04/11 12:55:18 by albaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
+
+# include "../koflibc/sources.h"
 
 #endif
